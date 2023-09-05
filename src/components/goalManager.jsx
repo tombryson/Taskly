6dddc160a92acd28fa94d92c3607bb98d0777990
tombryson <thomas.bryson@outlook.com>
2023-09-05 20:49:55 +1000

    added select project code for goals on frontend

--- src/components/goalManager.jsx
+++ src/components/goalManager.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const GoalManager = () => {
@@ -6,6 +6,8 @@ const GoalManager = () => {
     const [goalEndDate, setGoalEndDate] = useState(null);
     const [goalHours, setGoalHours] = useState(null);
     const [isGoalDivVisible, setIsGoalDivVisible] = useState(false);
+    const [selectedProject, setSelectedProject] = useState('');
+    const [projectNames, setProjectNames] = useState([]);
 
     const handleGoalStartDateChange = (e) => setGoalStartDate(e.target.value);
     const handleGoalEndDateChange = (e) => setGoalEndDate(e.target.value);
@@ -29,7 +31,6 @@ const GoalManager = () => {
 
     try {
         const response = await axios.post('https://pomodoro-analytics.fly.dev/updateGoal', goalData);
-
         if (response.status === 200) {
             console.log('Goal updated successfully:', response.data);
         }
@@ -39,34 +40,74 @@ const GoalManager = () => {
 
 };
 
-  const viewGoals = async () => {
+const viewGoals = async () => {
     try {
         const response = await axios.get('https://pomodoro-analytics.fly.dev/getGoals')
 
         if (response.status === 200) {
-            console.log(`Your goals: ${JSON.stringify(response.data, 2, null)}`);
+            let goals = JSON.stringify(response.data)
+            return (
+                <div>{goals}</div>
+            )
         }
     } catch (error) {
         console.error("We couldn't find any goals successfully");
     }
 }
 
+async function getProjectArray() {
+    let projectNames = [];
+    try {
+      const response = await axios.get('https://pomodoro-analytics.fly.dev/getGoals');
+      if (response.status === 200) {
+        projectNames = response.data.map(x => x.project);
+      }
+    } catch (error) {
+      console.error("Couldn't find goal data");
+    }
+    console.log(`projectNames: ${projectNames}`)
+    return projectNames;
+  }
+
+useEffect(() => {
+    (async () => {
+        const fetchedProjectNames = await getProjectArray();
+        setProjectNames(fetchedProjectNames);
+        console.log(fetchedProjectNames);
+    })();
+}, []);
+
+
+const handleProjectSelection = (e) => {
+    setSelectedProject(e.target.value);
+}
+
+
   return (
     <div>
-      <button onClick={toggleGoalDiv}>Goals</button>
-      <div id="analytics-goal-div" className={isGoalDivVisible ? '' : 'hidden'}>
-        <label htmlFor="goalStartDate">Start Date:</label>
-        <input type="date" id="goalStartDate" onChange={handleGoalStartDateChange} /><br />
+        <button onClick={toggleGoalDiv}>Goals</button>
+        <div id="analytics-goal-div" className={isGoalDivVisible ? '' : 'hidden'}>
+            <label htmlFor="project">Project: </label>
+            <select id="projectSelect" value={selectedProject} onChange={handleProjectSelection}>
+                <option value="" disabled>Select
+                </option>
+                {projectNames.map((project, index) => (
+                    <option key={index} value={index}>{project}</option>
+                ))}
+            </select>
 
-        <label htmlFor="goalEndDate">End Date:</label>
-        <input type="date" id="goalEndDate" onChange={handleGoalEndDateChange} /><br />
+            <label htmlFor="goalStartDate">Start Date: </label>
+            <input type="date" id="goalStartDate" onChange={handleGoalStartDateChange} /><br />
 
-        <label htmlFor="goalHours">Hours:</label>
-        <input type="number" id="goalHours" onChange={handleGoalHoursChange} /><br />
+            <label htmlFor="goalEndDate">End Date: </label>
+            <input type="date" id="goalEndDate" onChange={handleGoalEndDateChange} /><br />
 
-        <button onClick={viewGoals}>View Goals</button>
-        <button onClick={submitGoal}>Set Goal</button>
-      </div>
+            <label htmlFor="goalHours">Hours: </label>
+            <input type="number" id="goalHours" onChange={handleGoalHoursChange} /><br />
+
+            <button onClick={viewGoals}>View Goals</button>
+            <button onClick={submitGoal}>Set Goal</button>
+        </div>
     </div>
   );
 };
